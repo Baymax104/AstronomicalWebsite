13a6e1c33ca5fd519af8595d4bae29794bf72070
JohnWei <wzy1048168235@163.com>
2022-10-17 15:25:57 +0800

    添加侧边栏滚动粘滞

--- src/js/content.js
+++ src/js/content.js
@@ -4,23 +4,20 @@
     // 动态添加标签
     let sideBar = document.getElementsByTagName("side-bar")[0];
     for (let i = 0; i < h1.length; i++) {
-        let title = h1[i].innerHTML;
+        let target = h1[i];
+        // 设置a标签
         let a = document.createElement("a");
         a.setAttribute("href", "javascript:");
-        a.innerHTML = title;
-        sideBar.appendChild(a);
-    }
+        a.innerHTML = target.innerHTML;
 
-    let aLabels = sideBar.getElementsByTagName("a");
-    for (let i = 0; i < aLabels.length; i++) {
-        aLabels[i].index = i;
-        aLabels[i].onclick = function () {
-            let index = this.index;
-            let target = h1[index];
+        // 添加点击事件
+        a.addEventListener("click", function () {
             target.scrollIntoView({
                 behavior: "smooth",
                 block: "center"
             });
-        }
+        })
+
+        sideBar.appendChild(a);
     }
 })();
--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -19,23 +19,43 @@ sideBarTemp.innerHTML = `
             let shadow = this.attachShadow({mode: "open"});
             let content = sideBarTemp.content;
 
+            // 获取host下的a标签
+            let sideBar = document.getElementsByTagName("side-bar")[0];
+            let aLabels = sideBar.getElementsByTagName("a");
+            // 外部操作Shadow树则使用host对象的shadowRoot属性获取
+            // let shadowRoot = sideBar.shadowRoot;
+
+            // 添加a标签到sideBarList
+            let sideBarList = content.getElementById("side_bar_list");
+            while (aLabels.length !== 0) {
+                let sideBarItem = document.createElement("li");
+                sideBarItem.setAttribute("class", "side_bar_item");
+                sideBarItem.appendChild(aLabels[0]);
+                sideBarList.appendChild(sideBarItem);
+            }
+
+
+            // 添加滚动粘滞
+            // 计算不包含marginTop的偏移量
+            let container = document.getElementById("view_container");
+            let styleOfContainer = getComputedStyle(container);
+            let marginTop = parseInt(styleOfContainer.marginTop);
+            // offsetTop包含marginTop，将marginTop减去
+            let offsetTop = container.offsetTop - marginTop;
+
+            let fixedOffset = marginTop;
+            let sidebar = content.getElementById("side_bar");
+            window.addEventListener("scroll", function () {
+                if (window.scrollY > offsetTop) {
+                    sidebar.style.position = "fixed";
+                    sidebar.style.top = fixedOffset + "px";
+                } else {
+                    sidebar.style.position = "static";
+                }
+            });
+
             shadow.appendChild(content);
         }
     }
     window.customElements.define("side-bar", SideBar);
-
-    // 外部操作sideBar内元素
-    let sideBar = document.getElementsByTagName("side-bar")[0];
-    // 获取host下的a标签
-    let aLabels = sideBar.getElementsByTagName("a");
-    // 获取shadowRoot
-    let shadowRoot = sideBar.shadowRoot;
-
-    let sideBarList = shadowRoot.getElementById("side_bar_list");
-    while (aLabels.length !== 0) {
-        let sideBarItem = document.createElement("li");
-        sideBarItem.setAttribute("class", "side_bar_item");
-        sideBarItem.appendChild(aLabels[0]);
-        sideBarList.appendChild(sideBarItem);
-    }
 })();
