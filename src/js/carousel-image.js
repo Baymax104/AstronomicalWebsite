ab246419596b7cc48c0e3c11ed2fc5d2a119263e
JohnWei <wzy1048168235@163.com>
2022-10-18 15:49:58 +0800

    优化轮播图

--- src/js/carousel-image.js
+++ src/js/carousel-image.js
@@ -18,6 +18,34 @@ carouselTemp.innerHTML = `
 
 
 (function () {
+    class CarouselImage extends HTMLElement {
+        constructor() {
+            super();
+            let shadow = this.attachShadow({mode: "open"});
+            let content = carouselTemp.content.cloneNode(true);
+
+            // 获取外部img标签
+            let carouselImage = document.getElementsByTagName("carousel-image")[0];
+            let imgs = carouselImage.getElementsByTagName("img");
+            let imgContainer = content.getElementById("carousel_img_container");
+
+            // 复制第一个img标签用于补图
+            let firstLi = document.createElement("li");
+            let firstImg = imgs[0].cloneNode(true);
+            firstLi.appendChild(firstImg);
+            // 添加img
+            while (imgs.length !== 0) {
+                let li = document.createElement("li");
+                li.appendChild(imgs[0]);
+                imgContainer.appendChild(li);
+            }
+            imgContainer.appendChild(firstLi);
+
+            shadow.appendChild(content);
+        }
+    }
+    window.customElements.define("carousel-image", CarouselImage);
+
 
     // 移动动画函数
     let move = function (obj, attr, target, speed, callback) {
@@ -43,46 +71,16 @@ carouselTemp.innerHTML = `
         }, speed);
     }
 
-    class CarouselImage extends HTMLElement {
-        constructor() {
-            super();
-            let shadow = this.attachShadow({mode: "open"});
-            let content = carouselTemp.content.cloneNode(true);
-
-            shadow.appendChild(content);
-        }
-    }
-    window.customElements.define("carousel-image", CarouselImage);
-
-
-    // 将外部img标签添加到shadowRoot内
-    // 获取外部img标签
-    let carouselImage = document.getElementsByTagName("carousel-image")[0];
-    let imgs = carouselImage.getElementsByTagName("img");
-
-    // 操作shadowRoot
-    let shadowRoot = carouselImage.shadowRoot;
-    let imgContainer = shadowRoot.getElementById("carousel_img_container");
-
-    // 复制第一个img标签用于补图
-    let firstLi = document.createElement("li");
-    let firstImg = imgs[0].cloneNode(true);
-    firstLi.appendChild(firstImg);
-    // 添加img
-    while (imgs.length !== 0) {
-        let li = document.createElement("li");
-        li.appendChild(imgs[0]);
-        imgContainer.appendChild(li);
-    }
-    imgContainer.appendChild(firstLi);
-
-
     // 添加轮播
     let mainColor = "#0466be";
     let distance = 493;
 
-    imgs = imgContainer.getElementsByTagName("img");
-    let aContainer = shadowRoot.getElementById("carousel_nav");
+    let carousel = document.getElementsByTagName("carousel-image")[0].shadowRoot;
+
+    let imgContainer = carousel.getElementById("carousel_img_container");
+    let imgs = imgContainer.getElementsByTagName("img");
+
+    let aContainer = carousel.getElementById("carousel_nav");
     let aLabels = aContainer.getElementsByTagName("a");
 
     // 默认选中第一个
